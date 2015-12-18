8fb99969e80cc4f94e33b031b4bfe9908b61676a
Muharani Nabilla Rianti <muharani.nabilla.r@mail.ugm.ac.id>
2015-12-18 20:56:58 +0700

    add index.php file

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>Kereta Api</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">        
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    </head>
+
+<body style="background:#ecf0f1">			
+  	<div class="navbar navbar-fixed-top" style="position: absolute;">
+		<div class="navbar-inner">
+			<div class="container" style="background:lightblue;width: auto; padding: 0 20px;">
+				<a class="brand" href="index.php">Kereta Api Indonesia</a>
+					<ul class="nav">
+						<li class="active"><a href="home.php">Kereta Api</a></li>
+						<li><a href="kereta_api.php">Daftar Perjalanan</a></li>
+						<li><a href="kelas.php">Kelas Perjalanan</a></li>
+					
+					</ul>
+
+			</div>
+		</div>
+	</div>
+
+<div class="jumbotron">
+</br>
+
+</br>
+	<div align="center"><img src="img/logo.jpeg" width="70%"></div>
+  <h1 align="center">Selamat Datang </h1>
+  <p align="center">Di Kereta Api Indonesia</p>   	  
+</div>
+</div>
+  
+</div>
+<script src="js/jquery.js"></script>
+      <!-- Bootstrap javascript -->
+      <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
